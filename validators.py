9eaa53f6329c8e826b55a881cc9a9d752be8ac98
ylu1201 <128629717+ylu1201@users.noreply.github.com>
2023-04-01 00:28:31 +0200

    Validators for user_login.html

---
+++ validators.py
@@ -0,0 +1,12 @@
+from wtforms import Form, StringField
+from wtforms.validators import length, email
+
+
+class LoginForm(Form):
+    firstname = StringField(validators=[length(min=3, max=30,\
+                message="Please enter the right firstname!")])
+    
+    lastname = StringField(validators=[length(min=3, max=30,\
+                message="Please enter the right lastname!")])
+    
+    #email = StringField(validators=[email(message="Please enter the right email!")])
