434c62cc6ffd6b4c341554c2ac47364f8efe6690
ylu1201 <mrviredon@gmail.com>
2023-04-09 21:01:48 +0200

    Delete exts.py

--- exts.py
+++
@@ -1,5 +0,0 @@
-#To avoid circular references, third-party modules and files need to be placed in a separate file.
-#Eg. SQLAlchemy and Flask-Mail etc.
-
-from flask_sqlalchemy import SQLAlchemy
-db = SQLAlchemy()
